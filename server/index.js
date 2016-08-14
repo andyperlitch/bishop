b578ccee2af7d5b6593c542db797b31560024acc
Andrew Perlitch <perlitch@cisco.com>
2016-08-13 19:03:57 -0700

    WIP

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
@@ -8,27 +9,33 @@ require('dotenv').config({ path: path.resolve(__dirname, '..', '.env' ) });
 
 
 var app = express();
-app.use(function(req, res, next) {
-    var data = '';
-    req.setEncoding('utf8');
-    req.on('data', function(chunk) {
-        data += chunk;
-    });
-    req.on('end', function() {
-        req.rawBody = data;
-        next();
-    });
-});
-app.use(bodyParser());
+// app.use(function(req, res, next) {
+//     var data = '';
+//     req.setEncoding('utf8');
+//     req.on('data', function(chunk) {
+//         data += chunk;
+//     });
+//     req.on('end', function() {
+//         req.rawBody = data;
+//         next();
+//     });
+// });
+// app.use(bodyParser());
 
 app.post('/api/recognize-speech', function(req, res) {
   console.log('recognizing speech...');
+  // var writer = fs.createWriteStream('./sample' + Math.random() + '.wav', { defaultEncoding: 'binary'  });
+  // req.pipe(writer).on('end', function() {
+  //   res.end();
+  // });
   recognizer(req, function(err, result) {
     if (err) {
       res.status(400);
-      res.json(err);
+      res.send(JSON.stringify(err));
+      console.log('error occurred: ', err);
       return;
     }
+    console.log('success!', result);
     res.json(result);
   })
 });
--- server/recognizer.js
+++ server/recognizer.js
@@ -1,33 +1,114 @@
+var wav = require('wav');
+var through2 = require('through2');
+var SoxCommand = require('sox-audio');
 var google = require('googleapis');
 var getAuthClient = require('./getAuthClient');
 var speech = google.speech('v1beta1').speech;
+
+var addStandardListeners = function(command) {
+	command.on('prepare', function(args) {
+		console.log('Preparing sox command with args ' + args.join(' '));
+	});
+
+	command.on('start', function(commandLine) {
+		console.log('Spawned sox with command ' + commandLine);
+	});
+
+	command.on('progress', function(progress) {
+	    console.log('Processing progress: ', progress);
+	});
+
+	command.on('error', function(err, stdout, stderr) {
+	    console.log('Cannot process audio: ' + err.message);
+	    console.log('Sox Command Stdout: ', stdout);
+	    console.log('Sox Command Stderr: ', stderr)
+	});
+
+	command.on('end', function() {
+	    console.log('Sox command succeeded!');
+	});
+};
+
 function recognizer(request, cb) {
   getAuthClient(function(err, authClient) {
     if (err) {
       return cb(err);
     }
-    var requestPayload = {
-      config: {
-        encoding: 'LINEAR16',
-        sampleRate: 44100
-      },
-      audio: {
-        content: request.rawBody.toString('base64')
-      }
-    };
 
-    speech.syncrecognize({
-        auth: authClient,
-        resource: requestPayload
-      }, function (err, result) {
-        if (err) {
-          // console.log('error: ', err);
-          debugger;
-          return cb(err);
+    // Create a readable stream from wav
+    var reader = new wav.Reader();
+
+    // Create sox command and output stream
+    var soxOutput = through2(function(chunk, enc, callback) {
+      this.push(chunk);
+      console.log('pushing chunk...');
+      callback();
+    });
+    var command = SoxCommand()
+      .input(reader)
+      .inputFileType('wav')
+      .output(soxOutput)
+      .outputEncoding('signed-integer')
+      .outputBits(16)
+      .outputChannels(1)
+      .outputFileType('raw');
+    addStandardListeners(command);
+
+    // Set the listener on the format
+    reader.on('format', function(f) {
+
+      console.log('Creating sox output stream...');
+      console.log(f);
+      var data;
+      var format = f;
+      command
+        .inputSampleRate(f.sampleRate)
+        .inputBits(f.bitDepth)
+        .inputChannels(f.channels)
+
+      soxOutput.on('data', function(chunk) {
+        console.log('data received on soxOutput...');
+        if (data) {
+          data = Buffer.concat([data, chunk]);
         }
-        console.log('result:', JSON.stringify(result, null, 2));
-        cb(null, result);
+        else {
+          data = chunk;
+        }
       });
+
+      soxOutput.on('end', function() {
+        console.log('Sox output stream ended', data instanceof Buffer);
+        var requestPayload = {
+          config: {
+            encoding: 'LINEAR16',
+            sampleRate: format.sampleRate
+          },
+          audio: {
+            content: data.toString('base64')
+          }
+        };
+
+        speech.syncrecognize({
+            auth: authClient,
+            resource: requestPayload
+          }, function (err, result) {
+            if (err) {
+              return cb(err);
+            }
+            console.log('result:', JSON.stringify(result, null, 2));
+            cb(null, result);
+          });
+      });
+
+      command.run();
+
+    });
+
+    // Pipe request to the reader
+    console.log('Reading command and piping input...');
+
+    request.pipe(reader).pipe(soxOutput);
+
   });
 }
 
